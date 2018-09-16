76458b0ee39b2346a05acb827384b97cb55adf96
Ethan Chang <ethan_chang@icloud.com>
2018-09-15 18:23:43 -0700

    draws bounding box around contour, and tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/FalconVision/Detectors/Sampling.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/FalconVision/Detectors/Sampling.java
@@ -4,6 +4,7 @@ import org.firstinspires.ftc.teamcode.RobotFunctions.FalconVision.OpenCVpipeline
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -33,12 +34,13 @@ public class Sampling extends OpenCVpipeline {//TODO: crashes when program is st
         this.rgba = rgba;
         Imgproc.cvtColor(rgba, hsv, Imgproc.COLOR_RGB2HSV);
         Imgproc.blur(hsv, blurred, new Size(5, 5));
-        Core.inRange(blurred, new Scalar(15, 45, 75), new Scalar(45, 255, 255), filtered); //need to tune
+        Core.inRange(blurred, new Scalar(20, 40, 200), new Scalar(40, 255, 255), filtered); //need to tune
         Imgproc.findContours(filtered, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
         int i = 0;
         double maxArea = 0;
         double contourArea = 0;
+        Rect boundingRect;
         while(opmode.opModeIsActive() && i < contours.size()){ //made for loop out of while loop because I need to check if opmode is still running
             MatOfPoint cnts = contours.get(i);
             contourArea = Imgproc.contourArea(cnts);
@@ -52,7 +54,17 @@ public class Sampling extends OpenCVpipeline {//TODO: crashes when program is st
             i++;
         }
 
-        Imgproc.drawContours(rgba, contours, contourId, new Scalar(0, 255, 0), 3);
+        if(maxArea > 0){ //checks if there is a contour to draw
+            Imgproc.drawContours(rgba, contours, contourId, new Scalar(0, 255, 0), 3);
+            boundingRect = Imgproc.boundingRect(contours.get(contourId));
+            Imgproc.rectangle(rgba, boundingRect.tl(), boundingRect.br(), new Scalar(0, 0, 255), 3);
+            opmode.telemetry.addData("contour id", contours.get(contourId).size());
+        }
+
+
+
+        opmode.telemetry.update();
+
         contours.clear();
 
         return rgba;
