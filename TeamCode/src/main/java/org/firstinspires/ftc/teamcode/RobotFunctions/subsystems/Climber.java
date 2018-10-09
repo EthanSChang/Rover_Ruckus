fbded9aed3d95cce8cc19c5932885451dfbcd354
Ethan Chang <ethan_chang@icloud.com>
2018-10-08 19:40:25 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/subsystems/Climber.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.RobotFunctions.subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Climber {
+
+    public Climber(HardwareMap hmap, LinearOpMode linOpMode){
+
+    }
+}
