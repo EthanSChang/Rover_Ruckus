96733c0b0a918f8c7cf4d0cce66a17d4a07acbb2
Ethan Chang <ethan_chang@icloud.com>
2018-10-14 20:26:26 -0700

    added telemetry reports of encoder distance

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ArcadeDrive.java
@@ -5,6 +5,7 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.RobotFunctions.Calculators;
 import org.firstinspires.ftc.teamcode.RobotFunctions.TankHardware;
 
 import static org.firstinspires.ftc.teamcode.RobotFunctions.DashboardConstants.turnSpeed;
@@ -14,6 +15,7 @@ public class ArcadeDrive extends OpMode {
     TankHardware robot = new TankHardware();
     FtcDashboard dashboard = FtcDashboard.getInstance();
     TelemetryPacket packet = new TelemetryPacket();
+    Calculators calc = new Calculators();
 
     public void init(){
         robot.init(hardwareMap);
@@ -22,6 +24,7 @@ public class ArcadeDrive extends OpMode {
 
     public void loop(){
         robot.driveTrain.arcadeDrive(gamepad1.left_stick_x * turnSpeed, gamepad1.left_stick_y);
+        telemetry.addData("left dist", calc.Encoder2Inches(robot.driveTrain.bl.getCurrentPosition()));
         packet.put("back left pow", robot.driveTrain.bl.getPower());
         packet.put("back right pow", robot.driveTrain.br.getPower());
         dashboard.sendTelemetryPacket(packet);
