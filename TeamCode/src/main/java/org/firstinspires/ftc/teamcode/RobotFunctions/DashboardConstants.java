b8ef1fa477598b7c54b56b2c6bd27a6499094d06
Ethan Chang <ethan_chang@icloud.com>
2018-10-15 19:15:13 -0700

    added pid tuning constants for trajectory follower

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/DashboardConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/DashboardConstants.java
@@ -5,4 +5,11 @@ import com.acmerobotics.dashboard.config.Config;
 @Config
 public class DashboardConstants {
     public static double turnSpeed;
+    public static double HeadingKp;
+    public static double HeadingKi;
+    public static double HeadingKd;
+    public static double PathKp;
+    public static double PathKi;
+    public static double PathKd;
+
 }
