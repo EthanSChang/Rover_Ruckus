179f79bbe806983bf12ccdf81ca36bb6d13dc63c
Ethan Chang <ethan_chang@icloud.com>
2018-10-19 15:35:31 -0700

    changed to directly initialize climber mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ClimbTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ClimbTest.java
@@ -1,22 +1,25 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.RobotFunctions.TankHardware;
+import org.firstinspires.ftc.teamcode.RobotFunctions.subsystems.Climber;
 
 //this class allows a gamepad to control the climber, for testing encoder positions
 @TeleOp
-public class ClimbTest extends OpMode {
-    TankHardware robot = new TankHardware();
-    public void init(){
-        robot.init(hardwareMap);
-    }
+public class ClimbTest extends LinearOpMode {
 
-    public void loop(){
-        robot.climber.climb.setPower(gamepad1.left_stick_y);
-        telemetry.addData("motor power", robot.climber.climb.getPower());
-        telemetry.addData("motor encoder position", robot.climber.climb.getCurrentPosition());
-        telemetry.update();
+    public void runOpMode(){
+        Climber robot = new Climber(hardwareMap, this);
+        waitForStart();
+        while(opModeIsActive()){
+            robot.climb.setPower(gamepad1.left_stick_y);
+            telemetry.addData("motor power", robot.climb.getPower());
+            telemetry.addData("motor encoder position", robot.climb.getCurrentPosition());
+            telemetry.update();
+        }
+
     }
 }
