7a426dc0cbc33bcd4643cbb9be6251844be19ddf
Ethan Chang <ethan_chang@icloud.com>
2018-10-21 18:53:18 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/dashboardConstants/RoadRunnerConstants.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.RobotFunctions.dashboardConstants;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class RoadRunnerConstants {
+    public static double HeadingKp;
+    public static double HeadingKi;
+    public static double HeadingKd;
+    public static double PathKp;
+    public static double PathKi;
+    public static double PathKd;
+    public static double trackWidthSpeed = 0.3;
+    public static int trackWidthNumTurns = 4;
+}
