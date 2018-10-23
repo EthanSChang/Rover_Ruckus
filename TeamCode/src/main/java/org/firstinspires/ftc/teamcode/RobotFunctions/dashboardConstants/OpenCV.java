006a3d39f6853779c9cfc83def2843c836182689
Ethan Chang <ethan_chang@icloud.com>
2018-10-23 06:28:10 -0700

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/dashboardConstants/OpenCV.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.RobotFunctions.dashboardConstants;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class OpenCV {
+    public static double LowH = 20;
+    public static double LowS = 40;
+    public static double LowV = 200;
+    public static double HighH = 40;
+    public static double HighS = 255;
+    public static double HighV = 255;
+}
