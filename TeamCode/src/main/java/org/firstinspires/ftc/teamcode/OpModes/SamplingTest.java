bc349e9333e3abde51ba575e2bac5696cf875961
Ethan Chang <ethan_chang@icloud.com>
2018-10-28 11:27:28 -0700

    updated to read position correctly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SamplingTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SamplingTest.java
@@ -9,28 +9,51 @@ import org.firstinspires.ftc.teamcode.RobotFunctions.TheAllSeeingRobot.Detectors
 @Autonomous
 public class SamplingTest extends LinearOpMode {
     Sampling detector; //cannot write as Sampling detector = new Sampling();, will cause robot controller to crash in init
-    public int left, center, right, unknown;
-
-    public void runOpMode() throws InterruptedException { //currently crashes on init
+    int[] pos = new int[4];
+    public void runOpMode() throws InterruptedException {
         detector = new Sampling(); //need to add this piece during init
         detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance(), 0, this);
 
         waitForStart();
         detector.enable();
-
-
-        if(left>center && left>right && left>unknown){
-            telemetry.addData("measured position", "left");
-        } else if(center>left && center>right && left>unknown){
-            telemetry.addData("measured position", "center");
-        } else if(right>left && right>center && right>unknown){
-            telemetry.addData("measured position", "right");
-        } else{
-            telemetry.addData("measured position", "unknown");
+        while(opModeIsActive() && getRuntime() < 5){
+            telemetry.addData("status:", "running");
+            switch(detector.pos){
+                case 0: pos[0]++; break;
+                case 1: pos[1]++; break;
+                case 2: pos[2]++; break;
+                case 3: pos[3]++; break;
+            }
+            telemetry.addData("unknown", pos[0]);
+            telemetry.addData("left", pos[1]);
+            telemetry.addData("center", pos[2]);
+            telemetry.addData("right", pos[3]);
+            telemetry.update();
         }
-        telemetry.update();
 
-        while(opModeIsActive()){}
+        double max = 0;
+        int maxID = 0;
+
+        for(int i = 0; i < 4; i++){
+            if(pos[i] > max){
+                max = pos[i];
+                maxID = i;
+            }
+            if(!opModeIsActive()){break;}
+        }
+        telemetry.addData("max id", maxID);
+
+        switch(maxID){
+            case 0: telemetry.addData("detected position", "unknown");
+            case 1: telemetry.addData("detected position", "left");
+            case 2: telemetry.addData("detected position", "center");
+            case 3: telemetry.addData("detected position", "right");
+        }
+
+        telemetry.update();
+        while(opModeIsActive()){
+
+        }
 
         detector.disable();
     }
