bb97885e3ff2cdf7ab7d4f96b8d31f52fb2af119
Ethan Chang <ethan_chang@icloud.com>
2018-11-18 19:47:03 -0800

    modified to use cubing function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ArcadeDrive.java
@@ -23,7 +23,7 @@ public class ArcadeDrive extends OpMode {
     }
 
     public void loop(){
-        robot.driveTrain.arcadeDrive(gamepad1.left_stick_x * turnSpeed, gamepad1.left_stick_y);
+        robot.driveTrain.arcadeDrive(gamepad1.left_stick_x * turnSpeed, gamepad1.left_stick_y, true);
         telemetry.addData("left dist", calc.Encoder2Inches(robot.driveTrain.bl.getCurrentPosition()));
         packet.put("back left pow", robot.driveTrain.bl.getPower());
         packet.put("back right pow", robot.driveTrain.br.getPower());
