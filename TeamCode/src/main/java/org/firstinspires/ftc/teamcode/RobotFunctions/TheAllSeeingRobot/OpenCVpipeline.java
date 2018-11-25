0940ba6e764a740e79ae78dbe75f2161e582bb0e
Ethan Chang <ethan_chang@icloud.com>
2018-11-24 20:01:30 -0800

    modified to fix rotation issue using dogecv solution

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/TheAllSeeingRobot/OpenCVpipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/TheAllSeeingRobot/OpenCVpipeline.java
@@ -2,11 +2,13 @@ package org.firstinspires.ftc.teamcode.RobotFunctions.TheAllSeeingRobot;
 
 import android.app.Activity;
 import android.content.Context;
+import android.view.Surface;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 
 /**
@@ -84,8 +86,27 @@ public abstract class OpenCVpipeline implements CameraBridgeViewBase.CvCameraVie
     public void onCameraViewStarted(int width, int height) { // opencv needs it cause idk
     }
 
+    @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) { // I think it returns a mat with a camera frame input, idk
-        return processFrame(inputFrame.rgba(), inputFrame.gray());
+        Mat rgba = new Mat();
+        Mat gray = new Mat();
+
+        switch (((Activity) context).getWindowManager().getDefaultDisplay().getRotation()) {
+            case Surface.ROTATION_0:
+                // this breaks horribly for some reason
+                Core.rotate(inputFrame.rgba(), rgba, Core.ROTATE_90_CLOCKWISE);
+                Core.rotate(inputFrame.gray(), gray, Core.ROTATE_90_CLOCKWISE);
+                break;
+            case Surface.ROTATION_90:
+                rgba = inputFrame.rgba();
+                gray = inputFrame.gray();
+                break;
+            case Surface.ROTATION_270:
+                Core.rotate(inputFrame.rgba(), rgba, Core.ROTATE_180);
+                Core.rotate(inputFrame.gray(), gray, Core.ROTATE_180);
+                break;
+        }
+        return processFrame(rgba, gray);
     }
 
     public abstract Mat processFrame(Mat rgba, Mat gray); // is implemented in detector class and does the processing for the image
