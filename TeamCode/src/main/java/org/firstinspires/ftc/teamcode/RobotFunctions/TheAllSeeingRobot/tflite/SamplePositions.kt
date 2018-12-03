3715e86b3427df353e4d399a48cb8eb2ef5b7f59
Ethan Chang <ethan_chang@icloud.com>
2018-12-02 19:35:39 -0800

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/TheAllSeeingRobot/tflite/SamplePositions.kt
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.RobotFunctions.TheAllSeeingRobot.tflite
+
+/**
+ * Created by David Lukens on 10/31/2018.
+ */
+enum class SamplePositions {
+    LEFT,
+    CENTER,
+    RIGHT,
+    UNKNOWN
+}
