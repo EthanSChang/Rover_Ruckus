c55bbe9e1b113b0860a3ceeb6d0383da036e7bc8
Ethan Chang <ethan_chang@icloud.com>
2018-12-13 20:29:20 -0800

    added limit switches

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/subsystems/Climber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/subsystems/Climber.java
@@ -2,21 +2,27 @@ package org.firstinspires.ftc.teamcode.RobotFunctions.subsystems;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Climber {//TODO: add touch switch that only lets motor run to move climber up if switch is activated
     HardwareMap map;
     LinearOpMode linOpMode;
+    public DigitalChannel limLow, limHigh; //false when pressed
 
     public DcMotorEx climb; //positive power moves climber counterclockwise
     public Climber(HardwareMap hmap, LinearOpMode linOpMode){
         map = hmap;
         this.linOpMode = linOpMode;
         climb = (DcMotorEx) map.dcMotor.get("climb");
+        limLow = map.digitalChannel.get("limLow");
+        limHigh = map.digitalChannel.get("limHigh");
     }
 
     public Climber(HardwareMap hmap){
         map = hmap;
         climb = (DcMotorEx) map.dcMotor.get("climb");
+        limLow = map.digitalChannel.get("limLow");
+        limHigh = map.digitalChannel.get("limHigh");
     }
 }
