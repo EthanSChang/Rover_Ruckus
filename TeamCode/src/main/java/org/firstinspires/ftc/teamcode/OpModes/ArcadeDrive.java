e53db4b4ad88a5c8a8a8cd0b42741d3e516dc9c2
Ethan Chang <ethan_chang@icloud.com>
2019-01-02 16:41:04 -0800

    cleaned up, added climber control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ArcadeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ArcadeDrive.java
@@ -1,11 +1,8 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-import com.acmerobotics.dashboard.FtcDashboard;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.RobotFunctions.Calculators;
 import org.firstinspires.ftc.teamcode.RobotFunctions.TankHardware;
 
 import static org.firstinspires.ftc.teamcode.RobotFunctions.dashboardConstants.Misc.turnSpeed;
@@ -13,24 +10,58 @@ import static org.firstinspires.ftc.teamcode.RobotFunctions.dashboardConstants.M
 @TeleOp
 public class ArcadeDrive extends OpMode {
     TankHardware robot = new TankHardware();
-    FtcDashboard dashboard = FtcDashboard.getInstance();
-    TelemetryPacket packet = new TelemetryPacket();
-    Calculators calc = new Calculators();
+
+    boolean started = false; //has the robot started climbing
+    boolean rtPrevSt; //last state of right bumper (for lifiting)
+    double rtStick;
 
     public void init(){
         robot.init(hardwareMap);
         robot.driveTrain.setBrake();
     }
 
-    public void loop(){
-        robot.driveTrain.arcadeDrive(gamepad1.left_stick_x * turnSpeed, gamepad1.left_stick_y, gamepad1.left_bumper);
+    public void loop() {
+        //drivetrain control
+        robot.driveTrain.arcadeDrive(gamepad1.left_stick_x * turnSpeed, gamepad1.left_stick_y, gamepad1.a);
+
+        //intake control
         robot.intake.pivot.setPower(gamepad1.right_stick_y / 4);
-        if(gamepad1.left_bumper){robot.intake.flaps.setPower(1);}
-        else if (gamepad1.right_bumper){robot.intake.flaps.setPower(-1);}
-        else {robot.intake.flaps.setPower(0);}
-        telemetry.addData("left dist", calc.Encoder2Inches(robot.driveTrain.bl.getCurrentPosition()));
-        packet.put("back left pow", robot.driveTrain.bl.getPower());
-        packet.put("back right pow", robot.driveTrain.br.getPower());
-        dashboard.sendTelemetryPacket(packet);
+        if (gamepad1.left_bumper) {
+            robot.intake.flaps.setPower(1);
+        } else if (gamepad1.right_bumper) {
+            robot.intake.flaps.setPower(-1);
+        } else {
+            robot.intake.flaps.setPower(0);
+        }
+
+        //climber control
+
+        //lift robot automatically
+        if(gamepad1.right_bumper && !rtPrevSt && robot.climber.limLow.getState()) { //if bumper is pressed for the first time and the climber isn't already lifted
+            started = true;
+            robot.climber.climb.setPower(0.75); //start climbing
+        } else if(gamepad1.right_bumper && !rtPrevSt && started && robot.climber.limLow.getState()) { //if button is pressed again after climbing is started
+            started = false; //stop climbing if button is pressed again
+            robot.climber.climb.setPower(0);
+        } else if(started && !robot.climber.limLow.getState()){ //is the robot done climbing
+            started = false; //stop climbing
+            robot.climber.climb.setPower(0);
+        }
+        rtPrevSt = gamepad1.right_bumper;
+
+        //manual climber control
+        rtStick = gamepad1.right_stick_y;
+
+        if(!robot.climber.limLow.getState() && rtStick > 0){ //if trying to lower and climber is already at low limit
+            rtStick = 0;
+        } else if(!robot.climber.limHigh.getState() && rtStick < 0){ //if trying to raise and climber is already at high limit
+            rtStick = 0;
+        }
+
+        robot.climber.climb.setPower(rtStick);
+
+
+
+
     }
 }
