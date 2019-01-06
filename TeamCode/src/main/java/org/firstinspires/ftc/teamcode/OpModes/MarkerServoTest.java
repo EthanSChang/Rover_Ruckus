bdb16d9c380d0d12f5bfb2f7185111931171ba7c
Ethan Chang <ethan_chang@icloud.com>
2019-01-06 10:21:35 -0800

    added servo control code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MarkerServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MarkerServoTest.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode.OpModes;
 
-public class MarkerServoTest {
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.RobotFunctions.TankHardware;
+
+@TeleOp
+public class MarkerServoTest extends OpMode {
+    TankHardware robot = new TankHardware();
+
+    public void init(){
+        robot.init(hardwareMap);
+    }
+    public void loop(){
+        robot.markerDropper.dropper.setPosition(gamepad1.right_stick_y);
+        telemetry.addData("position", gamepad1.right_stick_y);
+    }
 }
