88c96f37080c542feb8805e28a0a53d96584144a
Ethan Chang <ethan_chang@icloud.com>
2019-01-06 10:21:28 -0800

    added constructors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/subsystems/MarkerDropper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotFunctions/subsystems/MarkerDropper.java
@@ -1,4 +1,25 @@
 package org.firstinspires.ftc.teamcode.RobotFunctions.subsystems;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
 public class MarkerDropper {
+    HardwareMap map;
+    LinearOpMode linOpMode;
+
+    public Servo dropper;
+
+    public MarkerDropper(HardwareMap hmap, LinearOpMode linOpMode){
+        map = hmap;
+        this.linOpMode = linOpMode;
+        dropper = map.servo.get("dropper");
+    }
+
+    public MarkerDropper(HardwareMap hmap){
+        map = hmap;
+        dropper = map.servo.get("dropper");
+    }
 }
