388d29b265715eb73142da6e9820cf5c8ec51105
Ethan Chang <ethan_chang@icloud.com>
2019-01-06 20:14:44 -0800

    initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/autos/Land.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.OpModes.autos;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.RobotFunctions.TankHardware;
+import org.firstinspires.ftc.teamcode.RobotFunctions.roadrunner.TrajectoryRunner;
+
+
+@Autonomous(name = "land", group = "auto")
+public class Land extends LinearOpMode { //just lands robot and moves climber out of hook, then lowers climber
+    TankHardware robot = new TankHardware();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
+        TrajectoryRunner runner = new TrajectoryRunner(robot.driveTrain, this);
+
+        waitForStart();
+
+        robot.climber.raise();
+        runner.runTrajectory("turn90");
+        robot.climber.lower();
+
+    }
+}
